0d68e7b38473ea19f1e18769d327defbca179d9a
Raul-Duarte <raulduarte.info@gmail.com>
2020-12-13 21:55:17 -0300

    estruturaBase

--- src/components/Chart/index.js
+++ src/components/Chart/index.js
@@ -1,61 +1,77 @@
 import React from 'react';
-import { View, Text, Dimensions, StyleSheet} from 'react-native';
+import { View, Text, Dimensions, StyleSheet } from 'react-native';
 import {
     LineChart,
 } from "react-native-chart-kit";
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 // import { Container } from './styles';
 
 const Chart = () => {
     return (
-        <View style={styles.conteiner}>
-            <Text>Bezier Line Chart</Text>
-            {console.log(Dimensions.get("window").width)}
-            <LineChart
-                data={{
-                    labels: ["0.0 s", "1.0 s", "2.0 s", "3.0 s", "4.0 s", "5.0 s"],
-                    datasets: [
-                        {
-                            data: [
-                                Math.random() * 100,
-                                Math.random() * 100,
-                                Math.random() * 100,
-                                Math.random() * 100,
-                                Math.random() * 100,
-                                Math.random() * 100
-                            ]
+        <>
+            <View style={styles.icons} >
+                <Icon
+                    raised
+                    name='heartbeat'
+                    type='font-awesome'
+                    color='#f50'
+                    onPress={() => console.log('hello')} />
+                <Icon
+                    raised
+                    name='heartbeat'
+                    type='font-awesome'
+                    color='#f50'
+                    onPress={() => console.log('hello')} />
+            </View>
+            <View style={styles.chart}>
+                {console.log(Dimensions.get("window").width)}
+                <LineChart
+                    data={{
+                        labels: ["0.0 s", "1.0 s", "2.0 s", "3.0 s", "4.0 s", "5.0 s"],
+                        datasets: [
+                            {
+                                data: [
+                                    Math.random() * 100,
+                                    Math.random() * 100,
+                                    Math.random() * 100,
+                                    Math.random() * 100,
+                                    Math.random() * 100,
+                                    Math.random() * 100
+                                ]
+                            }
+                        ]
+                    }}
+                    width={Dimensions.get("window").width} // from react-native
+
+                    height={220}
+                    yAxisSuffix="kg"
+                    yAxisInterval={1} // optional, defaults to 1
+                    chartConfig={{
+                        backgroundColor: "#fff",
+                        backgroundGradientFrom: "#FFF",
+                        backgroundGradientTo: "#A8A8A8",
+                        decimalPlaces: 2, // optional, defaults to 2dp
+                        color: (opacity = 1) => `rgba(232, 74, 91, ${opacity})`,
+                        labelColor: (opacity = 1) => `rgba(232, 74, 91, ${opacity})`,
+                        style: {
+                            borderRadius: 16
+                        },
+                        propsForDots: {
+                            r: "6",
+                            strokeWidth: "2",
+                            stroke: "#FFF"
                         }
-                    ]
-                }}
-                width={Dimensions.get("window").width} // from react-native
-                
-                height={220}
-                yAxisSuffix="kg"
-                yAxisInterval={1} // optional, defaults to 1
-                chartConfig={{
-                    backgroundColor: "#fff",
-                    backgroundGradientFrom: "#FFF",
-                    backgroundGradientTo: "#A8A8A8",
-                    decimalPlaces: 2, // optional, defaults to 2dp
-                    color: (opacity = 1) => `rgba(232, 74, 91, ${opacity})`,
-                    labelColor: (opacity = 1) => `rgba(232, 74, 91, ${opacity})`,
-                    style: {
+                    }}
+                    bezier
+                    style={{
+                        marginVertical: 8,
+                        marginHorizontal: 0,
                         borderRadius: 16
-                    },
-                    propsForDots: {
-                        r: "6",
-                        strokeWidth: "2",
-                        stroke: "#e84a5b"
-                    }
-                }}
-                bezier
-                style={{
-                    marginVertical: 8,
-                    marginHorizontal:0,
-                    borderRadius: 16
-                }}
-            />
-        </View>
+                    }}
+                />
+            </View>
+        </>
     )
 }
 
@@ -63,8 +79,16 @@ export default Chart;
 
 
 const styles = StyleSheet.create({
-    conteiner:{
-        
+    chart: {
+
         // marginTop:20,
+    },
+    icons: {
+        margin:10,
+        alignItems:'center',
+        justifyContent:'space-between',
+        alignContent:'space-between'
+
+
     }
 })
--- src/components/StartTreiner/index.js
+++ src/components/StartTreiner/index.js
@@ -1,28 +1,65 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 // import { Container } from './styles';
 
 const StartTreiner = () => {
     return (
         <View>
-            <Text>
-                00m 00s
-            </Text>
-            <Text>
-                abdominal
-            </Text>
-            <Button
-                title="Solid Button"
-            />
-            <Button
-                title="Solid Button"
-            />
+            <View>
+                <Text>
+                    00m 00s
+                </Text>
+                <Button
+                    title="Abdominal"
+                    iconRight={true}
+                    type="outline"
+                    icon={
+                        <Icon
+                            style={styles.icon}
+                            name="arrow-right"
+                            size={15}
+                            color="black"
+                        />
+                    }
 
+                />
+            </View>
+            <View style={styles.buttons}>
+                <Button
+                    title="INICIAR"
+                    buttonStyle={styles.button}
+                />
+                <Button
+                    buttonStyle={styles.button}
+                    title="RELATÓRIOS"
+                />
+            </View>
         </View>
 
     )
 }
 
 export default StartTreiner;
+
+
+const styles = StyleSheet.create({
+    icon: {
+        // paddingLeft:20,
+
+    },
+    button: {
+        width: 150,
+        backgroundColor: '#e84a5b',
+        
+    },
+    buttons: {
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'space-between',
+        margin:20,
+
+    }
+})
